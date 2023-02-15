3777708ca67c105991673a171a68f5ddac73bce4
sutairusu <sutairusu@icloud.com>
2023-02-15 14:36:21 +0900

    Complete 2nd challenge.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,13 +1,29 @@
 class RestaurantsController < ApplicationController
   def index
+    @restaurants = Restaurant.all
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
+    # @review = Review.find(params[:restaurant_id])
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
   end
 end
